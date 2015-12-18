dec8aaed10ff10dabbe42c1fbc4af063c4e87bde
Raymond Micheau <raymicheau@yahoo.ca>
2015-12-18 11:50:44 -0500

    Fire weapon added
    Made by Nick

---
+++ Assets/Fire.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour
+{
+    public ParticleSystem fire;
+    public EnemyHealth enemyHP;
+    Collision FireArea;
+    bool shooting;
+    // Use this for initialization
+    void Start()
+    {
+        fire.Clear();
+        fire.Pause();
+        shooting = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            fire.Play();
+            shooting = true;
+
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            fire.Pause();
+            fire.Clear();
+            shooting = false;
+        }
+
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (shooting) {
+            if (col.gameObject.name == "Enemies") {
+                enemyHP.TakeDamage(10);
+            }
+        }
+
+    }
+}
